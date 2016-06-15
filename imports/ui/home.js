7818f984bf491a0e2c27edabcb05d7fa06521d90
John Reda <johnreda4@gmail.com>
2016-06-15 19:39:47 -0400

    Second Push

---
+++ imports/ui/home.js
@@ -0,0 +1,13 @@
+import '/home.html';
+
+Template.home.onCreated(function(){
+	console.log("LOADED");
+});
+
+Template.home.helpers({
+
+});
+
+Template.home.events({
+
+});
--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/ui/home.html'
 
 Meteor.startup(() => {
   // code to run on server at startup
